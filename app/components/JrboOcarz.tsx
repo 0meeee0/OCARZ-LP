624e9dab3b18903f3198fa2489264c29f634efa9
0meeee0 <mehdi74.id@gmail.com>
2025-01-07 18:00:14 +0100

    Style: hover style added

--- app/components/JrboOcarz.tsx
+++ app/components/JrboOcarz.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function JrboOcarz() {
   return (
     <div className="flex justify-center py-10 sm:py-20">
-        <button className="bg-orange-400 font-bold text-white py-4 sm:py-5 px-16 sm:px-24 md:px-32 text-2xl sm:text-4xl">
-          جربو أوكارز
-        </button>
-      </div>
-  )
+      <button className="bg-orange-400 font-bold text-white py-4 sm:py-5 px-16 sm:px-24 md:px-32 text-2xl sm:text-4xl rounded-lg transition-all duration-300 ease-in-out hover:bg-white hover:text-orange-400 border-2 border-orange-400 hover:border-orange-400 hover:scale-105g">
+        جربو أوكارز
+      </button>
+    </div>
+  );
 }
 
-export default JrboOcarz
+export default JrboOcarz;
