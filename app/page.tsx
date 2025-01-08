067a9cbadf971149c7413d3ab0ddba9f71cb87b8
0meeee0 <mehdi74.id@gmail.com>
2025-01-08 17:06:34 +0100

    Chore: reorganized code structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,18 @@
 "use client";
 
 import Navbar from "./components/layouts/Navbar";
-import Hero from "./components/sections/Hero";
-import Hdro from "./components/sections/Hdro";
-import Steps from "./components/sections/Steps";
-import Testimonials from "./components/sections/Testimonials";
-import NumberOfCars from "./components/sections/NumberOfCars";
-import Trending from "./components/sections/Trending";
+import Hero from "./components/homePage/sections/Hero";
+import Hdro from "./components/homePage/sections/Hdro";
+import Steps from "./components/homePage/sections/Steps";
+import Testimonials from "./components/homePage/sections/Testimonials";
+import NumberOfCars from "./components/homePage/sections/NumberOfCars";
+import Trending from "./components/homePage/sections/Trending";
 import Footer from "./components/layouts/Footer";
-import Pricing from "./components/sections/pricing/Pricing";
+import Pricing from "./components/homePage/sections/pricing/Pricing";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
       <Hero />
       <Hdro />
       <Steps />
